f705725d1803620e2fac1f4c962a25524818803d
Miguel Fernandez <miguel.fernandez@docplanner.com>
2023-01-27 12:25:45 +0100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home">
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const counter = ref(0)
+
+    return {
+      counter
+    }
+  }
+}
+</script>
+
+ <!-- <script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++
+    },
+    decreaseCounter() {
+      this.counter--
+    }
+  }
+}
+
+</script> -->
+
+
+<style>
+  .home {
+    text-align: center;
+    padding: 20px;
+  }
+
+  .btn, .counter {
+    font-size: 40px;
+    margin: 10px;
+  }
+</style>
