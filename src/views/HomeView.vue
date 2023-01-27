554a5491bab6dea5156902c05e057d3a7d4fd685
Miguel Fernandez <miguel.fernandez@docplanner.com>
2023-01-27 12:46:11 +0100

    add refs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="home">
+
+    <h3> {{ counterTitle }}</h3>
+
     <div>
       <button @click="decreaseCounter" class="btn">-</button>
       <span class="counter">{{ counter }}</span>
       <button @click="increaseCounter" class="btn">+</button>
     </div>
+
+    <div class="edit">
+      <h4>Edit counter title: </h4>
+      <input v-model="counterTitle" type="text">
+    </div>
+
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-    const counter = ref(0)
+    const counter = ref(0),
+     counterTitle = ref('My Counter')
     
     const increaseCounter = () => {
       counter.value++
@@ -76,5 +86,9 @@ export default {
   .btn, .counter {
     font-size: 40px;
     margin: 10px;
+  }
+
+  .edit {
+    margin-top: 60px;
   }
 </style>
