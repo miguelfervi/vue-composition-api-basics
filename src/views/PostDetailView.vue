dbee0017c95071a0ee99800536db0065270e95db
Miguel Fernandez <miguel.fernandez@docplanner.com>
2023-02-02 21:09:11 +0100

    add routes

---
+++ src/views/PostDetailView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="post-detail">
+    <h1>This is a post page</h1>
+    <p>Display the content of post with ID of {{ $route.params.id }} here!</p>
+    <p><RouterLink to="/posts">&lt; Back</RouterLink></p>  
+  </div>
+</template>
+
+<script setup>
+
+
+</script>
