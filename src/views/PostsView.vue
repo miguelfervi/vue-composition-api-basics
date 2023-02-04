ce5b456d3cf61073e7a3e5d826e8ea6f2370f58b
Miguel Fernandez <miguel.fernandez@docplanner.com>
2023-02-04 11:01:12 +0100

    use v-for

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -3,13 +3,8 @@
     <h1>Posts</h1>
 
     <ul>
-      <li>
-        <RouterLink to="/postDetail/id1">Post 1</RouterLink>
-      </li>
-       <li>
-        <RouterLink to="/postDetail/id2">Post 2</RouterLink>
-      </li> <li>
-        <RouterLink to="/postDetail/id3">Post 3</RouterLink>
+      <li v-for="post in posts" :key="post.id" >
+        <RouterLink :to="`/postDetail/${post.id}`">{{ post.title }}</RouterLink>
       </li>
     </ul>
 
@@ -18,8 +13,25 @@
 </template>
 
 <script setup>
+  import { ref } from 'vue'
   import { vAutofocus} from '@/directives/vAutofocus'
 
+
+  const posts = ref([
+    {
+      id: 'id1',
+      title: 'Post 1'
+    },
+    {
+      id: 'id2',
+      title: 'Post 2'
+    },
+    {
+      id: 'id3',
+      title: 'Post 3'
+    }
+
+  ])
 
 </script>
 
