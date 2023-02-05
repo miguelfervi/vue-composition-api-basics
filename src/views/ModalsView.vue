0ac421aa4be1623b261ad203b80cb35af2985d92
Miguel Fernandez <miguel.fernandez@docplanner.com>
2023-02-05 16:25:40 +0100

    add modal

--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,11 +2,13 @@
     <div class="modals">
         <h1>Modals</h1>
         <button @click="showModal = true">Show modal</button>
-        <div class="modal" v-if="showModal">
-            <h1>This is a modal</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque veritatis porro ea nemo debitis totam ab nisi saepe neque repellat repellendus fuga, ullam quod voluptatem cupiditate magnam error pariatur recusandae.</p>
-            <button @click="showModal = false">Hide modal</button>
-        </div>
+        <teleport to=".modals-container">
+            <div class="modal" v-if="showModal">
+                <h1>This is a modal</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque veritatis porro ea nemo debitis totam ab nisi saepe neque repellat repellendus fuga, ullam quod voluptatem cupiditate magnam error pariatur recusandae.</p>
+                <button @click="showModal = false">Hide modal</button>
+            </div>
+        </teleport>
     </div>
 </template>
 
@@ -20,5 +22,11 @@ const showModal = ref(false)
 .modal {
     background: beige;
     padding: 10px;
+    left: 0;
+    top: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
 }
 </style>
