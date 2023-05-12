2661d4e66276dc05d2acabeae70fe78390bc48a4
Miguel Fernandez <miguel.fernandez@docplanner.com>
2023-05-12 10:30:47 +0200

    add pinia

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,21 +3,21 @@
 
     <h2 ref="appTitleRef">{{ appTitle }}</h2>
 
-    <h3> {{ counterData.title }}</h3>
+    <h3>{{counter.title}}</h3>
 
     <div>
-      <button @click="decreaseCounter(2)" class="btn">--</button>
-      <button @click="decreaseCounter(1)" class="btn">-</button>
-      <span class="counter">{{ counterData.count }}</span>
-      <button @click="increaseCounter(1)" class="btn">+</button>
-      <button @click="increaseCounter(2)" class="btn">++</button>
+      <button @click="counter.decreaseCounter(2)" class="btn">--</button>
+      <button @click="counter.decreaseCounter(1)" class="btn">-</button>
+      <span class="counter">{{ counter.count }}</span>
+      <button @click="counter.increaseCounter(1)" class="btn">+</button>
+      <button @click="counter.increaseCounter(2)" class="btn">++</button>
     </div>
 
-    <p>This counter is {{ oddOrEven }}</p>
+    <p>This counter is {{ counter.oddOrEven }}</p>
 
     <div class="edit">
       <h4>Edit counter title: </h4>
-      <input v-model="counterData.title" type="text" v-autofocus>
+      <input v-model="counter.title" type="text" v-autofocus>
     </div>
 
   </div>
@@ -25,11 +25,11 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import { useCounter } from '@/use/useCounter'
+import { useCounterStore } from '@/stores/counter'
 import { vAutofocus} from '@/directives/vAutofocus'
 
 
-  const appTitle = 'My Amazing Counter App'
+  const appTitle = 'My Ok Counter App'
 
   const appTitleRef = ref(null)
 
@@ -37,7 +37,7 @@ import { vAutofocus} from '@/directives/vAutofocus'
     console.log(`The app title is ${appTitleRef.value.offsetWidth} px width`)
   })
 
-  const {counterData, oddOrEven, increaseCounter, decreaseCounter} = useCounter()
+  const counter = useCounterStore()
 
    
 </script>
